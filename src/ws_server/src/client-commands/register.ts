cb7d666757bea899cddfe58cb36eecfb5685af35
Constantine Kolobov <kolobovconstantine@gmail.com>
2025-11-09 14:40:34 +0100

    fix: handle same name error

--- src/ws_server/src/client-commands/register.ts
+++ src/ws_server/src/client-commands/register.ts
@@ -6,31 +6,38 @@ type Response = string;
 type NewPlayerId = string | null;
 
 export const register = (requestMessage: RegisterRequestMessage): [Response, NewPlayerId] => {
-  const { name, password } = requestMessage.data;
-  if (!name || !password) {
-    console.error('Invalid registration data');
-    const errorResponse: ResponseMessage = {
+  try {
+    const { name, password } = requestMessage.data;
+    if (!name || !password) {
+      throw new Error('Invalid registration data');
+    }
+    const newPlayer = playersDb.createPlayer({ name, password });
+    const response: ResponseMessage = {
       type: ClientMessageType.Registration,
       data: {
-        name: name ?? '',
-        index: '',
-        error: true,
-        errorText: 'Invalid registration data',
+        name: name,
+        index: newPlayer.id,
+        error: false,
+        errorText: '',
       },
       id: 0,
     };
-    return [makeResponseMessageString(errorResponse), null];
+    return [makeResponseMessageString(response), newPlayer.id];
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      const errorResponse: ResponseMessage = {
+        type: ClientMessageType.Registration,
+        data: {
+          name: '',
+          index: '',
+          error: true,
+          errorText: error.message,
+        },
+        id: 0,
+      };
+      return [makeResponseMessageString(errorResponse), null];
+    }
+    return ['Unknown server error', null];
   }
-  const newPlayer = playersDb.createPlayer({ name, password });
-  const errorResponse: ResponseMessage = {
-    type: ClientMessageType.Registration,
-    data: {
-      name: name,
-      index: newPlayer.id,
-      error: false,
-      errorText: '',
-    },
-    id: 0,
-  };
-  return [makeResponseMessageString(errorResponse), newPlayer.id];
 };
